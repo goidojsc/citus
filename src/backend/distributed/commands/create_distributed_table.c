2f65f0dd4586354696c90f9742f2e5c147885b14
Jelte Fennema <github-tech@jeltef.nl>
2020-02-17 13:55:29 +0100

    some extra asserts

--- src/backend/distributed/commands/create_distributed_table.c
+++ src/backend/distributed/commands/create_distributed_table.c
@@ -166,6 +166,7 @@ master_create_distributed_table(PG_FUNCTION_ARGS)
 	char *distributionColumnName = text_to_cstring(distributionColumnText);
 	Var *distributionColumn = BuildDistributionKeyFromColumnName(relation,
 																 distributionColumnName);
+	Assert(distributionColumn != NULL);
 	char distributionMethod = LookupDistributionMethod(distributionMethodOid);
 
 	CreateDistributedTable(relationId, distributionColumn, distributionMethod,
@@ -232,6 +233,7 @@ create_distributed_table(PG_FUNCTION_ARGS)
 	char *distributionColumnName = text_to_cstring(distributionColumnText);
 	Var *distributionColumn = BuildDistributionKeyFromColumnName(relation,
 																 distributionColumnName);
+	Assert(distributionColumn != NULL);
 	char distributionMethod = LookupDistributionMethod(distributionMethodOid);
 
 	char *colocateWithTableName = text_to_cstring(colocateWithTableNameText);
